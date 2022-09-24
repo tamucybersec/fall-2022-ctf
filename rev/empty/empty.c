74bb48214cfd599d3ce400ef11b772047829e7e1
Matthew L <maattt01@gmail.com>
2022-09-23 19:09:19 -0500

    nerfed empty

--- rev/empty/empty.c
+++ rev/empty/empty.c
@@ -4,20 +4,30 @@
 
 
 int main(int argc, char** argv) {
-	__asm__ (
-"push 0x4011be\n"
-"ret"
-	);
 
-	char flag[] = "\x0e\x00\x0e\x0c\x04\x12\x0f\x1c\x07\x07\x10\x36\x00\x07\x05\x00\x07\x0c\x36\x08\x1a\x04\x36\x0a\x06\x07\x0f\x1c\x1a\x00\x06\x07\x14";
-	
 	if (argc != 2) {
+        printf("Usage: %s <guess>\n", argv[0]);
 		exit(0);
 	}
+    
+    void* ptr = &&rethere;
+
+	__asm__ (
+    "push %0\n"
+    "ret"
+    :
+    : "g"(ptr)
+	);
+
+    rethere:
+	char flag[] = "\x0e\x00\x0e\x0c\x04\x12\x0f\x1c\x07\x07\x10\x36\x00\x07\x05\x00\x07\x0c\x36\x08\x1a\x04\x36\x0a\x06\x07\x0f\x1c\x1a\x00\x06\x07\x14";
+	
 	for (size_t i = 0; i < 33; i++) {
 		flag[i] = flag[i] ^ (char)0x69;
 	}
 	if(!strncmp(flag, argv[1], 33)) {
 		puts("correct!");
-	}
+	} else {
+        puts("incorrect!");
+    }
 }
